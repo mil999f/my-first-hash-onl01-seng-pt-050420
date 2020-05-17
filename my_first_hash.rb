935e72c3b7851499bcb356ae6c822094e0ee8e0f
Milly Flores <miflo0h@gmail.com>
2020-05-17 03:25:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash 
     my_hash = {"name" => "cat", "number" => "1", "age" => "1"}
-  # 
-
   
 end
 
@@ -10,8 +8,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
