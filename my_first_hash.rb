ea332e0416aa980f0dd365e1df1778d3af70fab4
Milly Flores <miflo0h@gmail.com>
2020-05-17 03:43:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,7 +19,10 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
- 
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
+  return shipping_manifest
+end
  
   
  
